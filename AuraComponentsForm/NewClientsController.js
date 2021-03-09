d11c3085af20afedd64b16e8f9fa509dae9931e3
DivineShout <33633854+DivineShout@users.noreply.github.com>
2021-03-10 01:09:39 +0200

    Update NewClientsController.js

--- AuraComponentsForm/NewClientsController.js
+++ AuraComponentsForm/NewClientsController.js
@@ -1,23 +1,22 @@
-({
-    handleCreateClient: function(component, event) {
-        var saveClientAction = component.get("c.addClient");
-            saveClientAction.setParams({
-                "client": component.get("v.newClient")
-            });
-        
-        saveClientAction.setCallback(this, function(response) {
-            var state = response.getState();
-            if(state === "SUCCESS") {
-                component.set("v.message", "Client created successfully");
-            }
-            else if (state === "ERROR") {
-                console.log('Problem saving client, response state: ' + state);
-            }
-            else {
-                console.log('Unknown problem, response state: ' + state);
-            }
-        });
- 
-        $A.enqueueAction(saveClientAction);
-    },
+({    
+    handleCreateClient : function(component, event, helper) {
+
+        var name = component.get('v.newClient.Name');
+        if(name == undefined || name == null || name == '') {
+         	helper.showToast('Fault!', 'Please fill up name', 'error');
+        }
+        else {
+        	var saveClientAction = component.get('c.addClient');
+        	saveClientAction.setParams({
+            	client : component.get('v.newClient')
+        	});
+        	saveClientAction.setCallback(this,function(response){
+                var state = response.getState();
+         		if(state === "SUCCESS") {
+  					helper.showToast('Success!', 'Record Inserted Successfully', 'success');
+         		}
+        	});
+        	$A.enqueueAction(saveClientAction);
+      	}	
+	}
 })
