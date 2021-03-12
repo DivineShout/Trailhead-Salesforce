1c703e81be677ca44ffcad68a683df6d59dd05cc
DivineShout <33633854+DivineShout@users.noreply.github.com>
2021-03-12 03:57:11 +0200

    Update ClientTableHelper.js

--- Aura Components/ClientTableHelper.js
+++ Aura Components/ClientTableHelper.js
@@ -1,12 +1,19 @@
 ({
-
- getClientList: function(component) {
- var action = component.get("c.getClients");
-
- action.setCallback(this, function(actionResult) {
-
- component.set("v.Clients", actionResult.getReturnValue()); 
- });
- $A.enqueueAction(action);
- }
+    getData : function(cmp) {
+        var action = cmp.get('c.getClients');
+        action.setParams({
+            limitSize : cmp.get('v.initialRows')
+        });
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set('v.data', response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        }));
+        $A.enqueueAction(action);
+    }
+       
 })
