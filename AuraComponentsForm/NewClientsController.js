54180be05aedb9a9faaa90c2e6360ce5ff1feef4
DivineShout <33633854+DivineShout@users.noreply.github.com>
2021-03-12 04:30:08 +0200

    Update NewClientsController.js

--- AuraComponentsForm/NewClientsController.js
+++ AuraComponentsForm/NewClientsController.js
@@ -1,14 +1,20 @@
 ({    
     handleCreateClient : function(component, event, helper) {
 
-        var name = component.get('v.newClient.Name');
+        var name = component.get('v.name');
+        var email = component.get('v.email');
+        var gender = component.get('v.gender');
+        var birthday = component.get('v.birthday');
         if(name == undefined || name == null || name == '') {
          	helper.showToast('Fault!', 'Please fill up name', 'error');
         }
         else {
-        	var saveClientAction = component.get('c.addClient');
+        	var saveClientAction = component.get('c.saveClient');
         	saveClientAction.setParams({
-            	client : component.get('v.newClient')
+            	name : name,
+                email : email,
+                gender : gender,
+                birthday : birthday
         	});
         	saveClientAction.setCallback(this,function(response){
                 var state = response.getState();
