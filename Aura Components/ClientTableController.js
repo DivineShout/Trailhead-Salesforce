579ea0a4d98e288fa82a1d7d9051c19c90d10790
DivineShout <33633854+DivineShout@users.noreply.github.com>
2021-03-12 03:58:47 +0200

    Update ClientTableController.js

--- Aura Components/ClientTableController.js
+++ Aura Components/ClientTableController.js
@@ -1,13 +1,21 @@
 ({
-   doInit : function(component, event, helper) {        
-      helper.getClientList(component);
-   },
+   init: function (cmp, event, helper) {
+        cmp.set('v.columns', [
+            { label: 'Name', fieldName: 'Name', type: 'text'},
+            { label: 'Email', fieldName: 'Email__c', type: 'text'},
+            { label: 'Gender', fieldName: 'Gender__c', type: 'text'},
+            { label: 'Birth Day', fieldName: 'Birth_Day__c', type: 'text'}
+        ]);
+        helper.getData(cmp);
+    },
+     
     
-    createClient : function(component, event, helper) {
+    
+    createClient: function (cmp, event, helper) {
         var recordEvent=$A.get("e.force:createRecord");
         recordEvent.setParams({
             "entityApiName": "Client__c"
         });
         recordEvent.fire();
- }
+    }
 })
